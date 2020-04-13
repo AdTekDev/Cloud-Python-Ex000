793ebb6abf4a87dd151c0df7b56bb2f3285f818a
AdTekDev <nguyenngtu777@gmail.com>
2020-04-13 12:13:46 +0700

    Add files via upload

---
+++ api-001.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask import jsonify
+
+db_connect = create_engine('sqlite:///dsNhanVien.db')
+app = Flask(__name__)
+api = Api(app)
+
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select * from NhanVien") # This line performs query and returns json result
+        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+
+class Employees_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from NhanVien where MaNV =%d "  %int(employee_id))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+        
+
+api.add_resource(Employees, '/employees') # Route_1
+api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_2
+
+
+if __name__ == '__main__':
+     app.run(port='8080')
